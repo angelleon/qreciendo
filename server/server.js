d72ecd0529286751590647a946758843f6d45893
Angel Leon <luianglenlop@gmail.com>
2019-12-04 10:36:21 -0600

    antes de la tragedia

--- server/server.js
+++ server/server.js
@@ -1,36 +1,39 @@
-let app = require('express')(),
-server = require('http').Server(app),
-bodyParser = require('body-parser')
-express = require('express'),
-cors = require('cors'),
-http = require('http'),
-path = require('path');
- 
-let comentarioRoute = require('./route/comentario');
-let util = require('./util/util');
- 
+let app = require("express")(),
+server = require("http").Server(app),
+bodyParser = require("body-parser");
+(express = require("express")),
+(cors = require("cors")),
+(http = require("http")),
+(path = require("path"));
+
+let comentarioRoute = require("./route/comentario");
+let util = require("./util/util");
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false }));
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors());
- 
-app.use(function(err, req, res, next) {
-return res.send({ "statusCode": util.statusCode.ONE, "statusMessage": util.statusMessage.SOMETHING_WENT_WRONG });
+
+app.use((err, req, res, next) => {
+    res.send({
+        statusCode: util.statusCode.ONE,
+        statusMessage: util.statusMessage.SOMETHING_WENT_WRONG
+    });
 });
- 
-app.use('/comentario', comentarioRoute);
- 
+
+app.use("/comentario", comentarioRoute);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-next();
+app.use((req, res, next) => {
+    next();
 });
- 
+
 /*first API to check if server is running*/
-app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, '../server/client/dist/index.html'));
-})
- 
- 
-server.listen(3000,function(){
-console.log('app listening on port: 3000');
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../server/client/dist/index.html"));
+});
+
+const port = 4200;
+server.listen(port, () => {
+    console.log("app listening on port: " + port);
 });
